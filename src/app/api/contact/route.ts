c8c8563da521612b9db831ccb433a740482928bb
Ranbir Verma <ranbirverma5911@gmail.com>
2025-06-08 01:39:57 +0530

    change env vars

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -9,6 +9,8 @@ export async function POST(req: Request) {
     const message = formData.get('message')
     const auditionFile = formData.get('audition')
 
+    console.log( process.env.EMAIL_USER, process.env.EMAIL_PASS, process.env.EMAIL_TO,"env vars")
+
     if (typeof name !== 'string' || typeof message !== 'string') {
       return NextResponse.json({ error: 'Missing or invalid fields' }, { status: 400 })
     }
@@ -29,15 +31,15 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.NEXT_PUBLIC_EMAIL_USER!,
-        pass: process.env.NEXT_PUBLIC_EMAIL_PASS!,
+        user: process.env.EMAIL_USER!,
+        pass: process.env.EMAIL_PASS!,
       },
     })
 
     // Mail options
     const mailOptions = {
-      from: `"Audition Submission" <${process.env.NEXT_PUBLIC_EMAIL_USER}>`,
-      to: process.env.NEXT_PUBLIC_EMAIL_TO,
+      from: `"Audition Submission" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_TO,
       subject: `New Audition from ${name}`,
       text: `${name} says:\n\n${message}`,
       ...(attachments.length > 0 && { attachments }),
